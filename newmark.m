8b5799fdf626d3f5f64d0ae4a4988635e1a3c17e
bouhnome <62355679+bouhnome@users.noreply.github.com>
2020-04-02 21:38:41 +0200

    Update newmark.m

--- newmark.m
+++ newmark.m
@@ -1,4 +1,4 @@
-function [tt,Xt,dXt]=newmark(X0,dX0,ddX0,t_init,dt,t_tot)
+function [tt,Xt,dXt]=newmark(X0,dX0,t_init,dt,t_tot)
 
 global k m Ma g l Io omega1 omega2 omega eps1 eps2 alpha beta lamda0
 global M C K %matrices-coefficients du formalisme de newmark 
