e4e823db0c9a12944870797d482a758b8bb01487
bouhnome <62355679+bouhnome@users.noreply.github.com>
2020-04-02 15:06:18 +0200

    Create calc_Fnl.m
    Ce fichier contient la fonction permettant de calculer l'application Fnl touts les éléments nécessaires à l'écriture de son expression sont donnés soit en argument soit sont des variables globales. Pour référence veuillez regarder l'expression de Fnl écrite dans la description de l'issue associée.

---
+++ calc_Fnl.m
@@ -0,0 +1,10 @@
+function Fnl = calc_Fnl(X,dX,ddX,lambdapp)
+global k m Ma g l Io omega1 omega2 omega eps1 eps2 alpha beta lamda0
+global M C K %matrices-coefficients du formalisme de newmark 
+
+Fnl = zeros(size(X));
+
+Fnl(1) = [;];%ecrire l'expression de Fnl ici
+
+
+end 
