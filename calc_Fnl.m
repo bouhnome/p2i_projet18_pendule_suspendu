b82cc4e3aba0f151a3149c55bd88cc632f43220a
bouhnome <62355679+bouhnome@users.noreply.github.com>
2020-04-07 00:24:53 +0200

    Update calc_Fnl.m

--- calc_Fnl.m
+++ calc_Fnl.m
@@ -1,11 +1,11 @@
 function Fnl = calc_Fnl(X,dX,t)
 
-global l0 a omega1 omega2 epsilon1 epsilon2 omega;
+global l0 a omega1 omega2 eps1 eps2 omega;
 Fnl=zeros(size(X));
 lambdapp=-a*omega^2*sin(omega*t);
 %
-Fnl(1)=-X(1)*dX(2)^2+2*epsilon1*omega1*dX(1)+omega1^2*(X(1)-1)-omega2^2*cos(X(2))+cos(X(2))*lambdapp/l0;
-Fnl(2)=(2*dX(1)*dX(2)+2*epsilon2*omega2*dX(2)+omega2^2*sin(X(2))-sin(X(2))*lambdapp/l0)/X(1);
+Fnl(1)=-X(1)*dX(2)^2+2*eps1*omega1*dX(1)+omega1^2*(X(1)-1)-omega2^2*cos(X(2))+cos(X(2))*lambdapp/l0;
+Fnl(2)=(2*dX(1)*dX(2)+2*eps2*omega2*dX(2)+omega2^2*sin(X(2))-sin(X(2))*lambdapp/l0)/X(1);
 
 end
 
