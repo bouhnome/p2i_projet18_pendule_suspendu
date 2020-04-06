c35485162cce920af06e762f5bab3b9cc20be3b4
Chetanveer <61321991+Chetanveer@users.noreply.github.com>
2020-04-06 21:18:06 +0200

    Add files via upload

--- calc_dFnl.m
+++ calc_dFnl.m
@@ -1,1 +1,20 @@
+function [dFX dFdX] = calc_dFnl(X,dX,t)
+
+global l0 a omega1 omega2 epsilon1 epsilon2 omega;
+
+dFX=zeros(length(X));
+dFdX=zeros(length(X));
+lambdapp=-a*omega^2*sin(omega*t);
+
+dFX(1,1)=-dX(2)^2+omega1^2;
+dFX(1,2)=omega2^2*sin(X(2))-sin(X(2))*lambdapp/l0;
+dFX(2,1)=-(2*dX(1)*dX(2)+2*epsilon2*omega2*dX(2)+omega2^2*sin(X(2))-sin(X(2))*lambdapp/l0)/(X(1)^2);
+dFX(2,2)=(omega2^2*cos(X(2))-cos(X(2))*lambdapp/l0)/X(1);
+
+dFdX(1,1)=2*epsilon1*omega1;
+dFdX(1,2)=-2*X(1)*dX(2);
+dFdX(2,1)=2*dX(2)/X(1);
+dFdX(2,2)=(2*dX(1)+2*epsilon2*omega2)/X(1);
+
+end
 
--- newmark.m
+++ newmark.m
@@ -1,4 +1,55 @@
 function [tt,Xt,dXt]=newmark(X0,dX0,t_init,dt,t_tot)
 
-global k m Ma g l Io omega1 omega2 omega eps1 eps2 alpha beta lamda0
-global M C K %matrices-coefficients du formalisme de newmark 
+global l0 a omega1 omega2 epsilon1 epsilon2 omega;
+% global knl f0 OMEG
+global M C K
+
+precNR=1.e-11;
+% sol init
+t=t_init;n=1;
+X=X0;dX=dX0;
+
+Fnl=zeros(size(X));
+dFX=zeros(length(X));
+dFdX=zeros(length(X));
+P=zeros(size(X));
+% P=f0*cos(OMEG*t);
+Fnl=calc_Fnl(X,dX,t);
+ddX=M\(P-C*dX-K*X-Fnl);
+tt(n)=t;
+Xt(:,n)=X;
+dXt(:,n)=dX;
+% integration temporelle
+for t=t_init+dt:dt:t_tot;   %Boucle sur les pas de temps
+    n=n+1;
+    % prediction
+    iter=0;
+    X=X+dt*dX+(dt^2/2)*ddX;
+    dX=dX+dt*ddX;
+    ddX=ddX;
+    % Calcul du residu
+    P=0;
+%     P=f0*cos(OMEG*t);
+    Fnl=calc_Fnl(X,dX,t);
+    res=P-M*ddX-C*dX-K*X-Fnl;
+    normres=norm(res)/norm(P);
+    while (normres>precNR);    %Newton Raphson
+        iter=iter+1;
+        % Calcul de la Jacobienne
+        [dFX dFdX]=calc_dFnl(X,dX,t);
+        J=(4/dt^2)*M+(2/dt)*(C+dFdX)+K+dFX;
+        % Calcul de la correction
+        deltaX=J\res;
+        X=X+deltaX;
+        dX=dX+(2/dt)*deltaX;
+        ddX=ddX+(4/dt^2)*deltaX;
+        % Calcul du residu
+        Fnl=calc_Fnl(X,dX,t);
+        res=P-M*ddX-C*dX-K*X-Fnl;
+%         normres=norm(res)/norm(P);
+        normres=norm(deltaX)/norm(X);
+    end
+    tt(n)=t;
+    Xt(:,n)=X;
+    dXt(:,n)=dX;
+end
