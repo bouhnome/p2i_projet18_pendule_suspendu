927c46369c2917414eac22e7452d3ba0f93f12aa
bouhnome <62355679+bouhnome@users.noreply.github.com>
2020-05-05 09:34:18 +0200

    Add files via upload

--- main.m
+++ main.m
@@ -4,15 +4,15 @@ clear all; close all;
 global k m Ma g l Io eps1 eps2 a omega1 omega2 omega0 alpha BETA M C K
 
 %%les constantes du problème
-k = 0.2;%2 fois la constante de raideur. (chaque ressort a une raideur de k/2)
-m = 0.5;%masse de la tige(solide S2)
-Ma = 5;%masse du solide S1
+k = 300;%2 fois la constante de raideur. (chaque ressort a une raideur de k/2)
+m = 0.1;%masse de la tige(solide S2)
+Ma = 0.5;%masse du solide S1
 g =9.81 ;%valeur du champs de gravité
-l = 5;%la moitie la longueure de la tige(solide S2)
+l = 0.1;%la moitie la longueure de la tige(solide S2)
 Io = 4*m*(l^2)/3;%moment d'inertie de la tige par rapport à son axe de rotation
 eps1 = 0.005;%facteur d'amortissement visqueux lié à omega1
 eps2 = 0.005;%facteur d'amortissement visqueux li2 à omega2
-a = 0;%amplitude de l'exitation d'entree 
+a = 0.1;%amplitude de l'exitation d'entree 
 omega1 = sqrt(k /(m+Ma));%pulsation du système masse ressort (pendule immobile)
 omega2 = sqrt(m*g*l/Io);%pulsation de résonnance du pendule seul
 omega0 = (omega1 + omega2)/2;%pulsation de l'exitation  
@@ -28,16 +28,16 @@ K = [omega1^2,0;0,0];
 
 %% Simulation pour le régime non-linéaire
 periode=2*pi/omega0;      % periode de l'excitation et de la reponse
-nb_pts_per=30;          % nb de points par periode pour l integration temporelle
+nb_pts_per=40;          % nb de points par periode pour l integration temporelle
 dt=periode/nb_pts_per;  % taille du pas de temps
-nb_per=30;              % nb de periodes pour le calcul temporel
+nb_per=70;              % nb de periodes pour le calcul temporel
 t_tot=nb_per*periode;   % temps final
 t_init=0;               % temps initial     
 
 %Le vecteur X est le vecteur des inconnues il contient z et theta 
 z0 = 0.5;%valeur de z initiale 
 zp0 = 0;%valeur de z_point initiale
-theta0 = 15*pi/180;%valeur de theta initiale 
+theta0 = 30*pi/180;%valeur de theta initiale 
 thetap0 = 0;% valeur de theta_point initiale
 X0=[z0;theta0];dX0=[zp0;thetap0];             % conditions initiales
          
@@ -45,16 +45,16 @@ X0=[z0;theta0];dX0=[zp0;thetap0];             % conditions initiales
 [tt,Xt,dXt]=newmark(X0,dX0,t_init,dt,t_tot);   % Integration par Newmark
 %% Simulation pour le régime linéaire
 periode=2*pi/omega0;      % periode de l'excitation et de la reponse
-nb_pts_per=30;          % nb de points par periode pour l integration temporelle
+nb_pts_per=40;          % nb de points par periode pour l integration temporelle
 dt=periode/nb_pts_per;  % taille du pas de temps
-nb_per=30;              % nb de periodes pour le calcul temporel
+nb_per=70;              % nb de periodes pour le calcul temporel
 t_tot=nb_per*periode;   % temps final
 t_init=0;               % temps initial
 
 %Le vecteur X est le vecteur des inconnues il contient z et theta 
 z0 = 0.5;%valeur de z initiale 
 zp0 = 0;%valeur de z_point initiale
-theta0 = 15*pi/180;%valeur de theta initiale 
+theta0 = 30*pi/180;%valeur de theta initiale 
 thetap0 = 0;% valeur de theta_point initiale
 X0=[z0;theta0];dX0=[zp0;thetap0];             % conditions initiales
 
@@ -68,7 +68,7 @@ X0=[z0;theta0];dX0=[zp0;thetap0];             % conditions initiales
 % zpAnaly = ;
 %% Plot et animation
 
-choix = 3; %variable choix prennant des valeurs de 1 à 3 pour choisir d'afficher la solution non-linéaire et la solution linéaire ou une comparaison des deux
+choix = 1; %variable choix prennant des valeurs de 1 à 3 pour choisir d'afficher la solution non-linéaire et la solution linéaire ou une comparaison des deux
 
 if choix ==1 %Affichage de la solution non-linéaire et comparaison avec ODE45
 %Plot de z
