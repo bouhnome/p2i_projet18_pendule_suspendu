9ca80760da01f11d175cefef1f31d16b34123b90
karinamartinez22 <62945646+karinamartinez22@users.noreply.github.com>
2020-05-19 11:12:53 +0200

    Update courbeReponse.m

--- courbeReponse.m
+++ courbeReponse.m
@@ -4,14 +4,14 @@ clear vars
 global k m Ma g l Io eps1 eps2 a omega1 omega2 omega0 alpha BETA M C K
 
 k = 0.2;%2 fois la constante de raideur. (chaque ressort a une raideur de k/2)
-m = 0.5;%masse de la tige(solide S2)
-Ma = 5;%masse du solide S1
+m = 0.1;%masse de la tige(solide S2)
+Ma = 0.5;%masse du solide S1
 g =9.81 ;%valeur du champs de gravité
-l = 5;%la moitie la longueure de la tige(solide S2)
+l = 0.1;%la moitie la longueure de la tige(solide S2)
 Io = 4*m*(l^2)/3;%moment d'inertie de la tige par rapport à son axe de rotation
 eps1 = 0.005;%facteur d'amortissement visqueux lié à omega1
 eps2 = 0.005;%facteur d'amortissement visqueux li2 à omega2
-a = 0;%amplitude de l'exitation d'entree 
+a = 0.05;%amplitude de l'exitation d'entree 
 omega1 = sqrt(k /(m+Ma));%pulsation du système masse ressort (pendule immobile)
 omega2 = sqrt(m*g*l/Io);%pulsation de résonnance du pendule seul
 omega0 = (omega1 + omega2)/2;%pulsation de l'exitation  
@@ -22,8 +22,8 @@ M = [1,0;0,l];
 C = [2*eps1*omega1,0;0,2*eps2*omega2*l];
 K = [omega1^2,0;0,0];
 
-nb_pts_per=30;          % nb de points par periode pour l integration temporelle
-nb_per=30;              % nb de periodes pour le calcul temporel
+nb_pts_per=70;          % nb de points par periode pour l integration temporelle
+nb_per=70;              % nb de periodes pour le calcul temporel
 t_init=0;               % temps initial  
 
 omegaf=5;domega=0.05;
@@ -31,7 +31,7 @@ omegaf=5;domega=0.05;
 % conditions initiales
 z0 = 0.5;%valeur de z initiale 
 zp0 = 0;%valeur de z_point initiale
-theta0 = 15*pi/180;%valeur de theta initiale 
+theta0 = 45*pi/180;%valeur de theta initiale 
 thetap0 = 0;% valeur de theta_point initiale
 X0=[z0;theta0];
 dX0=[zp0;thetap0]; 
@@ -55,4 +55,3 @@ end
 plot(OME,W,'r-o')    % courbe de reponse
 title('Courbe de reponse')
 xlabel('Omega');ylabel('Amplitude max');
-
