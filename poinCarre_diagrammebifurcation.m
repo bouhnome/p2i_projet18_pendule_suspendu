1aa3893de9f955818397b65fb39f21225077e2bb
karinamartinez22 <62945646+karinamartinez22@users.noreply.github.com>
2020-05-19 11:08:38 +0200

    update

--- poinCarre_diagrammebifurcation.m
+++ poinCarre_diagrammebifurcation.m
@@ -4,14 +4,13 @@ clear vars
 global k m Ma g l Io eps1 eps2 a omega1 omega2 omega0 alpha BETA M C K
 
 k = 0.2;%2 fois la constante de raideur. (chaque ressort a une raideur de k/2)
-m = 0.5;%masse de la tige(solide S2)
-Ma = 5;%masse du solide S1
+m = 0.1;%masse de la tige(solide S2)
+Ma = 0.5;%masse du solide S1
 g =9.81 ;%valeur du champs de gravité
-l = 5;%la moitie la longueure de la tige(solide S2)
+l = 0.1;%la moitie la longueure de la tige(solide S2)
 Io = 4*m*(l^2)/3;%moment d'inertie de la tige par rapport à son axe de rotation
 eps1 = 0.005;%facteur d'amortissement visqueux lié à omega1
-eps2 = 0.005;%facteur d'amortissement visqueux li2 à omega2
-a = 0;%amplitude de l'exitation d'entree 
+eps2 = 0.005;%facteur d'amortissement visqueux li2 à omega2 
 omega1 = sqrt(k /(m+Ma));%pulsation du système masse ressort (pendule immobile)
 omega2 = sqrt(m*g*l/Io);%pulsation de résonnance du pendule seul
 omega0 = (omega1 + omega2)/2;%pulsation de l'exitation  
@@ -28,11 +27,9 @@ scz=get(0,'screensize');
 % ne nombre de point
 % amplitude initiale, finale et increment d'amplitude pour les diagrammes des bifuraction
 % ne nombre de point d'incrementation de l'amplitude
-% af=0.1; ne=30; da=(af-a)/(ne-1); ne=300;
-ai=0.01; af=0.15; ne=200; da=(af-ai)/(ne-1); 
-% ai=0.0217; af=0.0218; ne=10; da=(af-ai)/(ne-1); 
+ai=0.05; af=0.1; ne=200; da=(af-ai)/(ne-1); 
 %
-
+a=ai;
 f1=omega1/(2*pi);
 T1=1/f1;
 %
@@ -48,7 +45,7 @@ end
 % Position initiale
 z0 = 0.5;%valeur de z initiale 
 zp0 = 0;%valeur de z_point initiale
-theta0 = 15*pi/180;%valeur de theta initiale 
+theta0 = 45*pi/180;%valeur de theta initiale 
 thetap0 = 0;% valeur de theta_point initiale
 X0=[z0;theta0];
 dX0=[zp0;thetap0]; 
@@ -62,7 +59,7 @@ T=2*pi/omega0;
 % ns nombre de periode d'integration a partir de laquelle la reponse est consideree comme stable
 % ni nombre de point d'integration pour la plus petite periode
 % nth nombre de periode de trace de la reponse temporelle en theta
-np=60; ns=30; ni=60; nth=60;
+np=1500; ns=800; ni=100; nth=100;
 % balayage en temps
 t0=0; tf=np*T; dt=T/ni; t_balayage=t0:dt:tf;
 %
@@ -83,31 +80,28 @@ for k=1:ne
     nt=length(z);
     na=nt-(np-ns)*ni;
     %
-    xh=z.*sin(theta);
-    zv=z.*cos(theta);
+    xh=z.*tan(theta);
     %
-    % trajectoire
+%     % trajectoire
     figure(hfg(1));
     set(hfg(1),'position',[10 50 scz(3)/4 scz(4)/3]);
-    plot(xh(na:nt),-zv(na:nt),'Ob');
+    plot(xh(na:nt),-z(na:nt),'Ob');
     xlabel('x (m)')
     ylabel('z (m)')
     axis([-0.1 0.1 -0.5 0.5]);
     title('positions successives de la masse du pendule');
     drawnow
     %
-    % theta sur les nth dernieres periodes
+%     % theta sur les nth dernieres periodes
     figure(hfg(2));
     set(hfg(2),'position',[10 150+scz(4)/3 scz(3)/4 scz(4)/3]);
     plot(tt(nt-nth*ni:nt),theta(nt-nth*ni:nt))
     xlabel('t (s)')
     ylabel('\theta (rd)')
-    axis([(nt-nth*ni)*dt tf -0.25 0.25]);
+    axis([(nt-nth*ni)*dt tf -0.00025 0.00025]);
     title('position angulaire')
     drawnow
 %
-%     thetap=x(:,4);
-%     lp=x(:,2);
     thetap=dXt(2,:);
     zp=dXt(1,:);
 %
@@ -127,7 +121,7 @@ for k=1:ne
     % cacul de l'amplitude de la reponse e partir du modele lineaire
     % ls longuer statique et la amplitude dynamique
     % amplitude de dl/dt en multipliant par -omega
-    axis([-0.6 0.6 -0.25 0]);
+    axis([-0.1 1.6 -4.5 0]);
     title('section de Poincare "en z"')
     drawnow
 %  
@@ -136,7 +130,7 @@ for k=1:ne
     plot(xp(:),xpp(:),'.b');
     xlabel('\theta (rd)')
     ylabel('d\theta/dt (rd/s)')
-    axis([-0.06 0.06 -0.06 0.06]);
+    axis([-1.2 1.2 -6.0 6.0]);
     title('section de Poincare "en theta"')
     drawnow
     
@@ -147,8 +141,8 @@ for k=1:ne
     plot(amp,yp,'.b');
     xlabel('a (m)')
     ylabel('z (m)')
-    axis([0 ne*da -0.6 0.6]);
-    title('diagramme de bifircation "en z"')
+    axis([ai ai+ne*da -0.6 0.6]);
+    title('diagramme de bifurcation "en z"')
     drawnow
     hold on
 %
@@ -157,8 +151,8 @@ for k=1:ne
     plot(amp,xp,'.b');
     xlabel('a (m)')
     ylabel('\theta (rd)')
-    axis([0 ne*da -0.04 0.04]);
-    title('diagramme de bifircation "en theta"')
+    axis([ai ai+ne*da -0.04 0.04]);
+    title('diagramme de bifurcation "en theta"')
     drawnow
     hold on
 %
