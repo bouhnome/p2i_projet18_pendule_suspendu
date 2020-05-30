7ecec61181936b1e349648245448f9a449c84470
karinamartinez22 <62945646+karinamartinez22@users.noreply.github.com>
2020-05-30 19:22:17 +0200

    Update poinCarre_diagrammebifurcation.m

--- poinCarre_diagrammebifurcation.m
+++ poinCarre_diagrammebifurcation.m
@@ -9,10 +9,10 @@ Ma = 0.5;%masse du solide S1
 g =9.81 ;%valeur du champs de gravité
 l = 0.1;%la moitie la longueure de la tige(solide S2)
 Io = 4*m*(l^2)/3;%moment d'inertie de la tige par rapport à son axe de rotation
-eps1 = 0.005;%facteur d'amortissement visqueux lié à omega1
-eps2 = 0.005;%facteur d'amortissement visqueux li2 à omega2 
+eps1 = 0.003;%facteur d'amortissement visqueux lié à omega1
+eps2 = 0.002;%facteur d'amortissement visqueux li2 à omega2 
 omega1 = sqrt(k /(m+Ma));%pulsation du système masse ressort (pendule immobile)
-omega2 = sqrt(m*g*l/Io);%pulsation de résonnance du pendule seul
+omega2 = sqrt(m*g*l/Io)/2;%pulsation de résonnance du pendule seul
 omega0 = (omega1 + omega2)/2;%pulsation de l'exitation  
 alpha = m/(m+Ma) ;%coefficient adimensionnel 
 BETA = m*l^2/Io;%coefficient adimensionnel 
@@ -27,15 +27,9 @@ scz=get(0,'screensize');
 % ne nombre de point
 % amplitude initiale, finale et increment d'amplitude pour les diagrammes des bifuraction
 % ne nombre de point d'incrementation de l'amplitude
-ai=0.05; af=0.1; ne=200; da=(af-ai)/(ne-1); 
+ai=0.05; af=0.1; ne=101; da=(af-ai)/(ne-1); 
 %
 a=ai;
-f1=omega1/(2*pi);
-T1=1/f1;
-%
-
-f2=omega2/(2*pi);
-T2=1/f2;
 % 
 hfg=zeros(1,10);
 for ii=1:6
@@ -51,28 +45,27 @@ X0=[z0;theta0];
 dX0=[zp0;thetap0]; 
 %
 % pulsation et periode d'excitation 
-omega=omega2*5/4;
-% omega=omega2*5.1/4;
 T=2*pi/omega0;
 %
 % np nombre de periode d'integration pour chaque solution temporelle
 % ns nombre de periode d'integration a partir de laquelle la reponse est consideree comme stable
 % ni nombre de point d'integration pour la plus petite periode
 % nth nombre de periode de trace de la reponse temporelle en theta
-np=1500; ns=800; ni=100; nth=100;
+np=500; ns=400; ni=40; nth=40;
 % balayage en temps
-t0=0; tf=np*T; dt=T/ni; t_balayage=t0:dt:tf;
+t0=0; tf=np*T; dt=T/ni;
 %
 %
+
 for k=1:ne
     % incrementaion de a de 1 mm
     a=a+da;
     %
     % integration des equations de mouvement
-    [tt,Xt,dXt]=newmarklin(X0,dX0,t0,dt,tf);
+    [tt,Xt,dXt]=newmark(X0,dX0,t0,dt,tf);
     %
     % longueur et position angulaire du pendule
-    z=Xt(1,:);
+    z=Xt(1,:)*Io;
     theta=Xt(2,:);
     %
     % nt nombre de points d'integration 
@@ -80,30 +73,27 @@ for k=1:ne
     nt=length(z);
     na=nt-(np-ns)*ni;
     %
-    xh=z.*tan(theta);
     %
-%     % trajectoire
+%   position
     figure(hfg(1));
     set(hfg(1),'position',[10 50 scz(3)/4 scz(4)/3]);
-    plot(xh(na:nt),-z(na:nt),'Ob');
-    xlabel('x (m)')
+    plot(tt(nt-nth*ni:nt),z(nt-nth*ni:nt));
+    xlabel('t (s)')
     ylabel('z (m)')
-    axis([-0.1 0.1 -0.5 0.5]);
-    title('positions successives de la masse du pendule');
+    title('z');
     drawnow
     %
-%     % theta sur les nth dernieres periodes
+%   theta sur les nth dernieres periodes
     figure(hfg(2));
     set(hfg(2),'position',[10 150+scz(4)/3 scz(3)/4 scz(4)/3]);
     plot(tt(nt-nth*ni:nt),theta(nt-nth*ni:nt))
     xlabel('t (s)')
     ylabel('\theta (rd)')
-    axis([(nt-nth*ni)*dt tf -0.00025 0.00025]);
     title('position angulaire')
     drawnow
 %
     thetap=dXt(2,:);
-    zp=dXt(1,:);
+    zp=dXt(1,:)*Io;
 %
 %   construction des sections de Poincare par echantillonnage tous les ni
 %   points des ns derniere periodes
@@ -112,16 +102,12 @@ for k=1:ne
     yp=z(na:ni:nt);
     ypp=zp(na:ni:nt);
 % 
-% trace des sections de Poincare pour theta et l
+% trace des sections de Poincare pour theta et z
     figure(hfg(3));
     set(hfg(3),'position',[3*scz(3)/4-10 50 scz(3)/4 scz(4)/3]);
     plot(yp(:),ypp(:),'.b');
     xlabel('z (m)')
     ylabel('dz/dt (m/s)')
-    % cacul de l'amplitude de la reponse e partir du modele lineaire
-    % ls longuer statique et la amplitude dynamique
-    % amplitude de dl/dt en multipliant par -omega
-    axis([-0.1 1.6 -4.5 0]);
     title('section de Poincare "en z"')
     drawnow
 %  
@@ -130,32 +116,28 @@ for k=1:ne
     plot(xp(:),xpp(:),'.b');
     xlabel('\theta (rd)')
     ylabel('d\theta/dt (rd/s)')
-    axis([-1.2 1.2 -6.0 6.0]);
     title('section de Poincare "en theta"')
     drawnow
     
-    amp(1:length(yp))=a;
+    amp(1:length(ypp))=a;
     % trace du diagramme de bifurcation
     figure(hfg(5));
     set(hfg(5),'position',[2*scz(3)/4-100 50 scz(3)/4 scz(4)/3]);
-    plot(amp,yp,'.b');
+    plot(amp,ypp,'.b');
+    axis([ai ai+ne*da -1.6 1.6]);
     xlabel('a (m)')
     ylabel('z (m)')
-    axis([ai ai+ne*da -0.6 0.6]);
     title('diagramme de bifurcation "en z"')
     drawnow
     hold on
 %
     figure(hfg(6));
     set(hfg(6),'position',[2*scz(3)/4-100 150+scz(4)/3 scz(3)/4 scz(4)/3]);
-    plot(amp,xp,'.b');
+    plot(amp,xpp,'.b');
     xlabel('a (m)')
     ylabel('\theta (rd)')
-    axis([ai ai+ne*da -0.04 0.04]);
     title('diagramme de bifurcation "en theta"')
     drawnow
     hold on
 %
 end
-
-
